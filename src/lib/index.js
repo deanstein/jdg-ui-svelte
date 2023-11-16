ea79b6fc2fecf1761961c5a4e9ed52d48d188544
deanstein <jdeangoldstein@gmail.com>
2023-11-15 23:07:34 -0700

    Initializing Svelte library

---
+++ src/lib/index.js
@@ -0,0 +1,8 @@
+import JDGButton from './JDG-Button.svelte';
+export { JDGButton };
+
+import JDGHeader from './JDG-Header.svelte';
+export { JDGHeader };
+
+import JDGHeaderNav from './JDG-Header.svelte';
+export { JDGHeaderNav };
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script></script>
+
+<h1>JDG UI SVELTE</h1>
+
+<style></style>
