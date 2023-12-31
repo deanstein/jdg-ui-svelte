7280e891530dd695668c7f509663816bebdf757f
deanstein <jdeangoldstein@gmail.com>
2023-12-31 12:07:12 -0700

    Adding load animation to horizontal stripes

--- src/lib/JDGStripesHorizontal.svelte
+++ src/lib/JDGStripesHorizontal.svelte
@@ -1,13 +1,18 @@
 <script>
-    import { css } from "@emotion/css";
-
+    import { css, keyframes } from "@emotion/css";
     import { jdgColors, jdgSizes } from "./jdg-styling-constants.js";
 
     export let stripeColors = jdgColors.accentStripesJDG;
-    
-	const headerStripeCss = css`
-		height: ${jdgSizes.headerStripeHeight};
-	`;
+
+    const fillAnimation = keyframes`
+        0% { width: 0; }
+        100% { width: 100%; }
+    `;
+
+    const headerStripeCss = css`
+        height: ${jdgSizes.headerStripeHeight};
+        animation: ${fillAnimation} 0.5s ease-out forwards;
+    `;
 </script>
 
 <div class="jdg-header-stripes-container">
@@ -18,7 +23,7 @@
 
 <style>
     .jdg-header-stripes-container {
-		display: flex;
-		flex-direction: column;
-	}
+        display: flex;
+        flex-direction: column;
+    }
 </style>
