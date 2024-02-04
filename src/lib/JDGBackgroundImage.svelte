9b716b692997bc4d3a26bb4f7389482b0b17d1bb
deanstein <jdeangoldstein@gmail.com>
2024-02-04 12:44:45 -0700

    Simpler approach for delaunay triangulation

--- src/lib/JDGBackgroundImage.svelte
+++ src/lib/JDGBackgroundImage.svelte
@@ -3,7 +3,7 @@
     import * as d3 from 'd3';
     import { Delaunay } from 'd3-delaunay';
   
-    export let numberOfPoints = 50;
+    // Default colors
     export let color1 = "#ccc";
     export let color2 = "#eee";
     export let color3 = "#fff";
@@ -14,26 +14,16 @@
         .attr("height", "100%");
   
       // Generate random points
-      const pixelRatio = window.devicePixelRatio || 1;
-      let points = d3.range(numberOfPoints).map(() => ({x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight}));
-  
-      // Create a force simulation and add forces to it
-      const simulation = d3.forceSimulation(points)
-        .force('x', d3.forceX().strength(0.02)) // Push nodes towards the center
-        .force('y', d3.forceY().strength(0.02)) // Push nodes towards the center
-        .force('collide', d3.forceCollide(20)); // Prevent nodes from overlapping
-  
-      // Run the simulation for a set number of iterations
-      for (let i = 0; i < 120; ++i) simulation.tick();
+      const points = d3.range(200).map(() => [Math.random() * window.innerWidth, Math.random() * window.innerHeight]);
   
       // Create Delaunay triangulation
-      const delaunay = Delaunay.from(points.map(d => [d.x, d.y]));
+      const delaunay = Delaunay.from(points);
       const triangles = delaunay.triangles;
   
       // Draw triangles
       for(let i = 0; i < triangles.length; i += 3) {
         svg.append('polygon')
-          .attr('points', `${points[triangles[i]].x},${points[triangles[i]].y} ${points[triangles[i+1]].x},${points[triangles[i+1]].y} ${points[triangles[i+2]].x},${points[triangles[i+2]].y}`)
+          .attr('points', `${points[triangles[i]]},${points[triangles[i+1]]},${points[triangles[i+2]]}`)
           .style('fill', [color1, color2, color3][Math.floor(Math.random() * 3)]); // Randomly select one of the prop colors
       }
     });
