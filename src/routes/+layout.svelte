86a2c023c7ac58fdaf269db1966356fc54a422ba
deanstein <jdeangoldstein@gmail.com>
2024-02-12 22:01:59 -0700

    Switching back to single color link treatment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,6 @@
 
 	import { jdgColors } from '$lib/jdg-styling-constants.js';
 	import { JDGBackground, JDGFooter, JDGHeader, JDGNotificationBanner } from '$lib/index.js';
-	import { convertHexToRGBA } from '$lib/jdg-utils.js';
 
 	// get the app version from package.json
 	//@ts-expect-error
@@ -12,22 +11,12 @@
 		'This is a UI library demo site. UI may load slowly and flash while loading because this is using raw UI library components, not compiled code.';
 
 	// global styles, but using emotion css
-	const hyperlinkColorOpacity = 0.75;
 	const demoContainerCss = css`
 		a {
 			color: ${jdgColors.text};
 		}
-		a.no-initial-underline::before {
-			background: linear-gradient(
-				to bottom,
-				${convertHexToRGBA(jdgColors.accentStripesJDG[0], hyperlinkColorOpacity)} 33%,
-				${convertHexToRGBA(jdgColors.accentStripesJDG[1], hyperlinkColorOpacity)} 33%,
-				${convertHexToRGBA(jdgColors.accentStripesJDG[1], hyperlinkColorOpacity)} 66%,
-				${convertHexToRGBA(jdgColors.accentStripesJDG[2], hyperlinkColorOpacity)} 66%
-			);
-		}
 		a:before {
-			background: ${jdgColors.accentStripesJDG[0]};
+			background-color: ${jdgColors.accentStripesJDG[0]};
 		}
 	`;
 </script>
@@ -35,7 +24,7 @@
 <div class="jdg-ui-demo-container {demoContainerCss}">
 	<JDGNotificationBanner message={disclaimerMessage} color={jdgColors.notificationInformation} />
 
-	<JDGHeader logoTitle={'JDG SVELTE UI'} logoSupertitle={'INTRODUCING'} useMobileNav={false} />
+	<JDGHeader logoTitle={'JDG SVELTE UI'} logoSupertitle={'INTRODUCING'} useMobileNav={true} />
 
 	<JDGBackground />
 
