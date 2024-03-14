ccb7a45f1f41beffcee7c3019d14ca43bc0b27b6
deanstein <jdeangoldstein@gmail.com>
2024-03-14 14:37:09 -0600

    Adding active border to carousel thumbnails

--- src/lib/components/JDGImageCarousel.svelte
+++ src/lib/components/JDGImageCarousel.svelte
@@ -1,8 +1,10 @@
 <script>
 	import { JDGImage, JDGImageTile } from '$lib/index.js';
+	import { jdgColors } from '$lib/jdg-styling-constants.js';
 
 	export let imageAttributeObjects = []; // all images shown in thumbnail collection
 	export let maxHeight = '50vh';
+	export let activeThumbnailColor = jdgColors.accentStripesJDG[0];
 
 	let activeImage = imageAttributeObjects[0]; // start with the first image
 
@@ -15,12 +17,19 @@
 	<JDGImage imageAttributes={activeImage} {maxHeight} cropToFit={false} />
 	<div class="carousel-thumbnail-container">
 		{#each imageAttributeObjects as imageAttributesObject, i}
-			<JDGImageTile
-				onClickFunction={() => setActiveImage(imageAttributesObject)}
-				imageAttributes={imageAttributesObject}
-				maxHeight="50px"
-				maxWidth="75px"
-			/>
+			<div
+				class="carousel-thumbnail-wrapper"
+				style={imageAttributesObject === activeImage
+					? `outline: 5px solid ${activeThumbnailColor}`
+					: ''}
+			>
+				<JDGImageTile
+					onClickFunction={() => setActiveImage(imageAttributesObject)}
+					imageAttributes={imageAttributesObject}
+					maxHeight="50px"
+					maxWidth="75px"
+				/>
+			</div>
 		{/each}
 	</div>
 </div>
@@ -37,5 +46,9 @@
 	.carousel-thumbnail-container {
 		display: flex;
 		gap: 1rem;
+	}
+
+	.carousel-thumbnail-wrapper {
+		box-sizing: border-box;
 	}
 </style>
