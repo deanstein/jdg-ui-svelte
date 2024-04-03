249e7b1d28eb1d82223e71709cb80096949b7a0b
deanstein <jdeangoldstein@gmail.com>
2024-04-02 23:27:34 -0600

    Setting grid layout to use flex for middle breakpoint

--- src/lib/components/JDGGridLayout.svelte
+++ src/lib/components/JDGGridLayout.svelte
@@ -4,17 +4,21 @@
 	import { jdgBreakpoints, jdgSizes } from '../jdg-styling-constants.js';
 	const gridContainerCss = css`
 		@media (max-width: ${jdgBreakpoints.width[0].toString() + jdgBreakpoints.unit}) {
+			display: grid;
 			gap: ${jdgSizes.contentBoxPaddingSm};
 			grid-template-columns: repeat(1, minmax(0, 1fr));
 		}
 		@media (min-width: ${jdgBreakpoints.width[0].toString() +
 			jdgBreakpoints.unit}) and (max-width: ${jdgBreakpoints.width[1].toString() +
 			jdgBreakpoints.unit}) {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: center;
 			gap: ${(jdgSizes.nContentBoxPaddingMd / 2).toString() +
 			jdgSizes.contentBoxFloatingMarginUnit};
-			grid-template-columns: repeat(2, minmax(0, 1fr));
 		}
 		@media (min-width: ${jdgBreakpoints.width[1].toString() + jdgBreakpoints.unit}) {
+			display: grid;
 			gap: ${(jdgSizes.nContentBoxPaddingLg / 2).toString() +
 			jdgSizes.contentBoxFloatingMarginUnit};
 			grid-template-columns: repeat(3, minmax(0, 1fr));
@@ -25,9 +29,3 @@
 <div class="jdg-grid-container {gridContainerCss}">
 	<slot />
 </div>
-
-<style>
-	.jdg-grid-container {
-		display: grid;
-	}
-</style>
