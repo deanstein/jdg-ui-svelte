c13788ed48d86d1f914d17dc872d45fee12afd76
deanstein <jdeangoldstein@gmail.com>
2024-04-07 23:47:14 -0600

    Attempt at animated compare slider

--- src/lib/components/JDGImageCompare.svelte
+++ src/lib/components/JDGImageCompare.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { writable } from 'svelte/store';
 	import imageAttributesCollection from '../../routes/image-attributes-collection.js';
 	import { JDGImage } from '$lib/index.js';
 
@@ -6,23 +7,44 @@
 	export let imageAttributes2 = imageAttributesCollection.ccp_blue_mall_80s90s_1;
 
 	let imageCompareContainerRef;
-	let sliderPosition = 50;
+	let isUserInteracting = false;
+	let sliderPositionStore = writable(50);
+	let animationId;
+
+	sliderPositionStore.subscribe((value) => {
+		if (!isUserInteracting) {
+			animationId = requestAnimationFrame(() => {
+				sliderPositionStore.set((value + 1) % 100);
+			});
+		}
+	});
+
+	function handleMouseEnter() {
+		isUserInteracting = true;
+		cancelAnimationFrame(animationId);
+	}
 
 	function handleMouseMove(event) {
 		const rect = imageCompareContainerRef.getBoundingClientRect();
 		const x = event.clientX - rect.left; //x position within the element.
-		sliderPosition = (x / rect.width) * 100;
+		sliderPositionStore.set((x / rect.width) * 100);
+	}
+
+	function handleMouseLeave() {
+		isUserInteracting = false;
 	}
 </script>
 
 <div
 	class="jdg-image-compare-container"
 	bind:this={imageCompareContainerRef}
+	on:mouseenter={handleMouseEnter}
 	on:mousemove={handleMouseMove}
+	on:mouseleave={handleMouseLeave}
 	role="slider"
 	aria-valuemin="0"
 	aria-valuemax="100"
-	aria-valuenow={sliderPosition}
+	aria-valuenow={$sliderPositionStore}
 	tabindex="0"
 >
 	<div class="compare-image" style="width: 100%;">
@@ -32,14 +54,17 @@
 			maxHeight="500px"
 		/>
 	</div>
-	<div class="compare-image" style="clip-path: inset(0 {100 - sliderPosition}% 0 0); width: 100%;">
+	<div
+		class="compare-image"
+		style="clip-path: inset(0 {100 - $sliderPositionStore}% 0 0); width: 100%;"
+	>
 		<JDGImage
 			imageAttributes={imageAttributes1}
 			alternateFitRef={imageCompareContainerRef}
 			maxHeight="500px"
 		/>
 	</div>
-	<div class="slider" style="left: {sliderPosition}%;"></div>
+	<div class="slider" style="left: {$sliderPositionStore}%;"></div>
 </div>
 
 <style>
