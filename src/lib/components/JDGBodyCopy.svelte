525272606dd517efb239dc9061ba97d9c3bec1c6
deanstein <jdeangoldstein@gmail.com>
2024-04-14 16:02:53 -0600

    Adding optional font size multiplier param to BodyCopy

--- src/lib/components/JDGBodyCopy.svelte
+++ src/lib/components/JDGBodyCopy.svelte
@@ -3,22 +3,24 @@
 
 	import { jdgBreakpoints, jdgSizes } from '$lib/jdg-styling-constants.js';
 
+	export let fontSizeMultiplier = 1; // optionally make fonts larger at all breakpoints
+
 	const bodyCopyContainerCss = css`
 		line-height: 1.5;
 		@media (max-width: ${jdgBreakpoints.width[0].toString() + jdgBreakpoints.unit}) {
-			font-size: 15px;
+			font-size: calc(${fontSizeMultiplier * 15}px;
 			padding: ${jdgSizes.contentBoxPaddingSm} ${jdgSizes.bodyCopyVerticalPaddingSm}
 				${jdgSizes.contentBoxPaddingSm} ${jdgSizes.bodyCopyVerticalPaddingSm};
 		}
-		@media (min-width: ${jdgBreakpoints.width[0].toString() +
-			jdgBreakpoints.unit}) and (max-width: ${jdgBreakpoints.width[1].toString() +
-			jdgBreakpoints.unit}) {
-			font-size: 16px;
+		@media (min-width: ${jdgBreakpoints.width[0].toString() + jdgBreakpoints.unit}) and (max-width: ${
+			jdgBreakpoints.width[1].toString() + jdgBreakpoints.unit
+		}) {
+			font-size: calc(${fontSizeMultiplier * 16}px;
 			padding: ${jdgSizes.contentBoxPaddingMd} ${jdgSizes.bodyCopyVerticalPaddingMd}
 				${jdgSizes.contentBoxPaddingMd} ${jdgSizes.bodyCopyVerticalPaddingMd};
 		}
 		@media (min-width: ${jdgBreakpoints.width[1].toString() + jdgBreakpoints.unit}) {
-			font-size: 18px;
+			calc(${fontSizeMultiplier * 18}px
 			padding: ${jdgSizes.contentBoxPaddingLg} ${jdgSizes.bodyCopyVerticalPaddingLg}
 				${jdgSizes.contentBoxPaddingLg} ${jdgSizes.bodyCopyVerticalPaddingLg};
 		}
