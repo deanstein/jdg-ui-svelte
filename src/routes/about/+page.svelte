84e5700624f06ac6a6c8f95a9159f54593a6c861
deanstein <jdeangoldstein@gmail.com>
2024-06-16 12:58:34 -0600

    Adding another full-width image example

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,16 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer } from '$lib/index.js';
+	import { JDGContentBoxFloating, JDGContentContainer, JDGImageFullWidth } from '$lib/index.js';
+	import { imageAttributesCollection } from '../image-attributes-collection.js';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating>ABOUT</JDGContentBoxFloating>
+<JDGContentContainer overlapWithHeader={true} topBottomPadding="0" gap="0">
+	<JDGImageFullWidth
+		imageAttributes={imageAttributesCollection.ccp_gold_mall_80s90s_1}
+		showOverlay={true}
+		overlayColorRgba="rgba(30, 30, 30, 0.5)"
+		primaryText="EXPERIENCE THE FAIRYTALE"
+		primaryTextFontFamily="Righteous"
+		secondaryText="at the Historic Englewood Museum"
+		secondaryTextFontFamily="Righteous"
+	/>
 </JDGContentContainer>
