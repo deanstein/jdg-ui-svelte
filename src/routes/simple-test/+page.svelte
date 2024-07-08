09764acabc092984d13f034f9c5a39ca0d6cb91b
deanstein <jdeangoldstein@gmail.com>
2024-07-08 16:47:26 -0600

    Adding simple-test page for streamlined debugging

---
+++ src/routes/simple-test/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import {
+		JDGBodyCopy,
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGImageTile
+	} from '$lib/index.js';
+	import { imageAttributesCollection } from '../image-attributes-collection.js';
+</script>
+
+<JDGContentContainer>
+	<JDGContentBoxFloating title="TEST SECTION">
+		<JDGBodyCopy paddingTop="0">
+			This page should serve as a place to test a single component that needs debugging. Keep it simple!
+		</JDGBodyCopy>
+		<JDGImageTile
+			imageAttributes={imageAttributesCollection.aerial_60s70s_1}
+			fillContainer={false}
+		/>
+		<JDGImageTile fillContainer={false} href="/about" />
+	</JDGContentBoxFloating>
+</JDGContentContainer>
