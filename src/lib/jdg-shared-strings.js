d3c1981c18dcf6e9fed86ec480c120b07cdaae19
deanstein <jdeangoldstein@gmail.com>
2024-07-13 20:28:47 -0600

    Created shared strings file, adding shared URLs from ccp-website

---
+++ src/lib/jdg-shared-strings.js
@@ -0,0 +1,10 @@
+export const urls = {
+	ccpFacebook: 'https://www.facebook.com/cinderellacityproject/',
+	ccpInstagram: 'https://www.instagram.com/cinderellacityproject/',
+	ccpYouTube: 'https://www.youtube.com/c/TheCinderellaCityProject',
+    ccpGitHubReleases: 'https://github.com/deanstein/CinderellaCityProject/tags',
+	historicEnglewood: 'https://historicenglewood.org/',
+	jdgLinkedIn: 'https://www.linkedin.com/in/jdeangoldstein/',
+	vipFacebook: 'https://www.facebook.com/villaitaliaproject/',
+	vipInstagram: 'https://www.instagram.com/villaitaliaproject/'
+};
