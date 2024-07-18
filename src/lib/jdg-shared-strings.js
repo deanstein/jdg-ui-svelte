691e809c314c1bc6dd2ab7df99cc9d89e5e1a711
deanstein <jdeangoldstein@gmail.com>
2024-07-17 22:50:15 -0600

    Cleanup

--- src/lib/jdg-shared-strings.js
+++ src/lib/jdg-shared-strings.js
@@ -4,8 +4,8 @@ export const jdgSharedUrls = {
 	ccpYouTube: 'https://www.youtube.com/c/TheCinderellaCityProject',
 	ccpGitHubReleases: 'https://github.com/deanstein/CinderellaCityProject/tags',
 	historicEnglewood: 'https://historicenglewood.org/',
-    jdgGitHub: 'https://github.com/deanstein',
-    jdgInstagram: 'https://www.instagram.com/jdeangoldstein/',
+	jdgGitHub: 'https://github.com/deanstein',
+	jdgInstagram: 'https://www.instagram.com/jdeangoldstein/',
 	jdgLinkedIn: 'https://www.linkedin.com/in/jdeangoldstein/',
 	vipFacebook: 'https://www.facebook.com/villaitaliaproject/',
 	vipInstagram: 'https://www.instagram.com/villaitaliaproject/'
