5a80ab7dd583b6b9440e0398d7b12df376e5cd9e
deanstein <jdeangoldstein@gmail.com>
2024-10-11 09:28:27 -0600

    Adding example local shared-strings

---
+++ src/routes/shared-strings.js
@@ -0,0 +1,8 @@
+const sharedStrings = {
+	websiteTitle: 'JDG UI SVELTE',
+	websiteIconSrc:
+		'https://res.cloudinary.com/jdg-main/image/upload/v1718070772/jdg-ui-svelte/jdg-ui-logo_cs4ji5.jpg',
+	websiteDescription: 'A demo page for JDG UI SVELTE.'
+};
+
+export default sharedStrings;
