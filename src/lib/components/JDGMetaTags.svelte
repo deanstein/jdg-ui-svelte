3eef935694fd5d9b0211a1b7021eaeb66af4bfa1
deanstein <jdeangoldstein@gmail.com>
2024-10-11 09:27:19 -0600

    Adding first pass of MetaTags component

---
+++ src/lib/components/JDGMetaTags.svelte
@@ -0,0 +1,31 @@
+<script>
+  // this component sets meta tags 
+  // and can be used both at the +layout.svelte and +page.svelte levels
+  // best practice is to use this at +layout.svelte and override when needed on +page.svelte
+
+	export let title1 = undefined; // for example, the website name
+	export let title2 = undefined; // for example, a page name
+	export let titleSeparator = ' | '; // used between title1 and title2
+	export let imageSrc = undefined; // URL or path to an image supporting the description
+  export let description = undefined; // short blurb about the page
+	export let faviconSrc = undefined; // primarily for the +layout.svelte level but can be used at +page.svelte too
+	export let type = 'website';
+
+	let combinedTitle;
+	if (title1 && title2) {
+		combinedTitle = title1 + titleSeparator + title2;
+	} else if (title1 && title1 !== '') {
+		combinedTitle = title1;
+	} else {
+		combinedTitle = title2;
+	}
+</script>
+
+<svelte:head>
+	{#if combinedTitle}<title>{combinedTitle}</title>{/if}
+	{#if combinedTitle}<meta property="og:title" content={combinedTitle} />{/if}
+	{#if imageSrc}<meta property="og:image" content={imageSrc} />{/if}
+	{#if description}<meta property="og:description" content={description} />{/if}
+	{#if faviconSrc}<link rel="icon" href={faviconSrc} />{/if}
+  <meta property="og:type" content={type} />
+</svelte:head>
