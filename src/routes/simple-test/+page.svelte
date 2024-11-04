f515ba58c3339cfbb91da7418c38bcef30dba2ed
deanstein <jdeangoldstein@gmail.com>
2024-11-03 23:31:24 -0700

    Updating simple demo page

--- src/routes/simple-test/+page.svelte
+++ src/routes/simple-test/+page.svelte
@@ -39,6 +39,31 @@
 					showCaption={true}
 					showAttribution={true}
 				/>
+				<JDGImageTile
+				imageAttributes={imageAttributesCollection.aerial_60s70s_1}
+				showCaption={true}
+				showAttribution={true}
+			/>
+			<JDGImageTile
+				imageAttributes={imageAttributesCollection.cc_1}
+				showCaption={true}
+				showAttribution={true}
+			/>
+			<JDGImageTile
+				imageAttributes={imageAttributesCollection.architecture_1}
+				showCaption={true}
+				showAttribution={true}
+			/>
+			<JDGImageTile
+				imageAttributes={imageAttributesCollection.rose_mall_60s70s_1}
+				showCaption={true}
+				showAttribution={true}
+			/>
+			<JDGImageTile
+				imageAttributes={imageAttributesCollection.rose_mall_60s70s_2}
+				showCaption={true}
+				showAttribution={true}
+			/>
 			</JDGGridLayout>
 		</JDGClipFade>
 	</JDGContentBoxFloating>
