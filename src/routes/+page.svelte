0c04947ddc3a98c8bb4764cddc123c4c1416165a
deanstein <jdeangoldstein@gmail.com>
2024-12-24 16:34:10 -0700

    Adding PMX example section to example home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -162,6 +162,32 @@
 				]}
 			/>
 		</JDGFullWidthContainer>
+		<div class="pmx-product-container-flex">
+			<div class="flex-container-left">
+				<JDGImageCarousel
+					imageAttributeObjects={[
+						imageAttributesCollection.aerial_60s70s_1,
+						imageAttributesCollection.architecture_1,
+						imageAttributesCollection.cc_2,
+						imageAttributesCollection.ccp_blue_mall_60s70s_1
+					]}
+					showCaption={false}
+					showAttribution={false}
+					showBlurInUnfilledSpace={false}
+				/>
+			</div>
+			<div class="flex-container-right">
+				<div class="right-panel-container">
+					The Denver Cityscape features over 70 of Denver's most iconic buildings at 1:5280 scale.
+					<br /><br />
+					Our custom algorithm approximates the building locations based on actual addresses, then adjusts
+					their locations on the platform to minimize overlap and maximize visibility.
+					<br /><br />
+					The Cityscape model is occasionally updated as Denver's skyline changes. Check the version
+					number on the bottom of the product and match it with the legend below.
+				</div>
+			</div>
+		</div>
 		<JDGBodyCopy textAlign="center" paddingBottom="0"
 			><b>IMAGE COMPARE</b>
 			<br />
@@ -535,3 +561,27 @@
 		item3ImageAttributes={imageAttributesCollection.ccp_blue_mall_80s90s_1}
 	/>
 </JDGContentContainer>
+
+<style>
+	.pmx-product-container-flex {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		gap: 20px;
+	}
+
+	.flex-container-left {
+		display: flex;
+		justify-content: right;
+	}
+
+	.flex-container-right {
+		flex: 0 0 30%;
+	}
+
+	.right-panel-container {
+		display: flex;
+		flex-direction: column;
+		gap: 40px;
+	}
+</style>
