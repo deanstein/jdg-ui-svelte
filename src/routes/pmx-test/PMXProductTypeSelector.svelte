a252a34f06c22caed28828272f5646ac0981843a
deanstein <jdeangoldstein@gmail.com>
2025-01-12 21:52:02 -0700

    Attempting to clone ProductTypeSelector

--- src/routes/pmx-test/PMXProductTypeSelector.svelte
+++ src/routes/pmx-test/PMXProductTypeSelector.svelte
@@ -3,13 +3,17 @@
 	import { afterUpdate } from 'svelte';
 
 	import { JDGImageTile } from '$lib/index.js';
-	import { appAccentColors } from '$lib/states/ui-state.js';
+	import { appAccentColors, imagesLoading } from '$lib/states/ui-state.js';
 
 	export let activeProductId = productTypeIds[0]; // optional - if not set, uses the first id
 	export let productTypeIds = []; // array of product IDs to show
 	export let productTypeThumbnailImageAttributes = []; // array of images for product types, in same order as IDs
 
+	let topLevelRef;
 	let productTypeSelectorRef;
+	let productTypeLoading = false;
+	let clonedContentRef;
+	const cloneId = 'matchStyles';
 
 	// show or hide the appropriate product type
 	const filterProductTypeDisplay = () => {
@@ -25,8 +29,34 @@
 	};
 
 	const setActiveProductType = (productTypeId) => {
+		const clonedElement = topLevelRef.cloneNode(true);
+		clonedContentRef.appendChild(clonedElement);
+		applyComputedStyles(topLevelRef, clonedContentRef, productTypeSelectorRef);
+
 		activeProductId = productTypeId;
 		filterProductTypeDisplay();
+
+		productTypeLoading = true;
+		setTimeout(() => {
+			productTypeLoading = false;
+		}, 100000);
+	};
+
+	const applyComputedStyles = (sourceElem, targetElem, parentElem) => {
+		// Apply styles only if the element is a descendant of the specified parent
+		console.log('Target:', targetElem.id, 'Source:', sourceElem.id);
+		if (targetElem.id === sourceElem.id) {
+			console.log('MATCH!!!');
+			const computedStyle = getComputedStyle(sourceElem);
+			for (let prop of computedStyle) {
+				targetElem.style[prop] = computedStyle.getPropertyValue(prop);
+			}
+
+			// Apply styles to children recursively
+			for (let i = 0; i < sourceElem.children.length; i++) {
+				applyComputedStyles(sourceElem.children[i], targetElem.children[i], parentElem.id);
+			}
+		}
 	};
 
 	// determine if the URL contains a hash
@@ -65,7 +95,7 @@
 	}
 </script>
 
-<div class="pmx-product-type-selector-container">
+<div bind:this={topLevelRef} class="pmx-product-type-selector-container">
 	<!-- type selector thumbnails -->
 	<div class="product-type-thumbnail-and-title-container">
 		<div class="product-type-title">Available models:</div>
@@ -96,18 +126,42 @@
 			{/each}
 		</div>
 	</div>
+
 	<!-- actual product type display -->
-	<div bind:this={productTypeSelectorRef} class="product-type-selector">
+	<div bind:this={productTypeSelectorRef} class="product-type-selector" id={cloneId}>
 		<slot />
+	</div>
+	<div
+		bind:this={clonedContentRef}
+		class="product-type-selector-clone-overlay"
+		style="display: {productTypeLoading ? 'block' : 'none'};"
+		id={cloneId}
+	>
+		{#if productTypeLoading}
+			<slot />
+		{/if}
 	</div>
 </div>
 
 <style>
 	.pmx-product-type-selector-container {
 		display: flex;
+		position: relative;
 		flex-direction: column;
 		justify-content: center;
 		gap: 50px;
+	}
+
+	.product-type-selector {
+		position: relative;
+	}
+
+	.product-type-selector-clone-overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-color: red;
+		z-index: 2;
 	}
 
 	.product-type-thumbnail-and-title-container {
