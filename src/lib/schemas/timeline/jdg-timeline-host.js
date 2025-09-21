9da37663a3f1d29b95640b496db1538888ea5e78
deanstein <jdeangoldstein@gmail.com>
2025-09-21 14:56:36 -0600

    Adding version and avatar to TimelineHost schema

--- src/lib/schemas/timeline/jdg-timeline-host.js
+++ src/lib/schemas/timeline/jdg-timeline-host.js
@@ -1,13 +1,17 @@
 // A timeline needs a host
 // like a person or building.
 
+import { jdgSchemaVersion } from "../jdg-schema-versions.js";
+
 // Hosts are expected to have at least these fields:
 const jdgTimelineHost = {
 	id: '',
 	name: '',
+	avatar: '',
 	timelineEvents: [],
 	timelineEventReferences: [],
-	contextualEvents: []
+	contextualEvents: [],
+	version: jdgSchemaVersion
 };
 
 export default jdgTimelineHost;
