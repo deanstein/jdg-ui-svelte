1f95feaeb7b0c770451bdc0ad0b7d558f41806ba
deanstein <jdeangoldstein@gmail.com>
2025-10-23 22:44:49 -0600

    Updating schema version to have only changes array, no overview

--- src/lib/schemas/jdg-schema-versions.js
+++ src/lib/schemas/jdg-schema-versions.js
@@ -1,13 +1,23 @@
 export const jdgSchemaVersion = '0.2.0';
 export const jdgSchemaChanges = {
-	'v0.2.x': {
+	'v0.3.x': {
 		Overview:
-			'v0.2.x adds inceptionDate and cessationDate to TimelineHost, removing inceptionEvent and cessationEvent from TimelineEvent.',
-		Changes: ['Added timeline event, timeline event content schemas']
+			'v0.3.x adds an ID field to image-meta schema and changes avatar to avatarImageMeta in timeilne-host schema',
+		Changes: [
+			'Added ID field to image-meta',
+			'Added avatarImageMeta field to timeline-host schema',
+			'Removed avatar field from timeline-host schema'
+		]
+	},
+	'v0.2.x': {
+		Changes: [
+			'Added inceptionDate and cessationDate to timeline-host schema',
+			'Removed inceptionEvent and cessationEvent from timeline-host schema'
+		]
 	},
 	'v0.1.x': {
-		Overview:
-			'v0.1.x adds the initial schema to JDG UI Svelte, starting with timeline event schemas.',
-		Changes: ['Added timeline event, timeline event content schemas']
+		Changes: [
+			'Added initial schema versioning to JDG UI Svelte, starting with timeline-host and timeline-event'
+		]
 	}
 };
