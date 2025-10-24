e2a3ca506acbaaa0292454d2fc1c3625d0cebcf7
deanstein <jdeangoldstein@gmail.com>
2025-10-23 22:45:45 -0600

    Prettier

--- src/lib/schemas/jdg-schema-versions.js
+++ src/lib/schemas/jdg-schema-versions.js
@@ -1,22 +1,20 @@
 export const jdgSchemaVersion = '0.2.0';
-export const jdgSchemaChanges = {
+export const jdgSchemachanges = {
 	'v0.3.x': {
-		Overview:
-			'v0.3.x adds an ID field to image-meta schema and changes avatar to avatarImageMeta in timeilne-host schema',
-		Changes: [
+		changes: [
 			'Added ID field to image-meta',
 			'Added avatarImageMeta field to timeline-host schema',
 			'Removed avatar field from timeline-host schema'
 		]
 	},
 	'v0.2.x': {
-		Changes: [
+		changes: [
 			'Added inceptionDate and cessationDate to timeline-host schema',
 			'Removed inceptionEvent and cessationEvent from timeline-host schema'
 		]
 	},
 	'v0.1.x': {
-		Changes: [
+		changes: [
 			'Added initial schema versioning to JDG UI Svelte, starting with timeline-host and timeline-event'
 		]
 	}
