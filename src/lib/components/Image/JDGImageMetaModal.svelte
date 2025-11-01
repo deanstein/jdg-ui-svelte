a6a5a574856a8b86396594d4887f291c4166c90f
deanstein <jdeangoldstein@gmail.com>
2025-10-31 19:34:17 -0600

    Adding remainder of image-meta schema fields to ImageMetaModal

--- src/lib/components/Image/JDGImageMetaModal.svelte
+++ src/lib/components/Image/JDGImageMetaModal.svelte
@@ -6,11 +6,17 @@
 	import { draftImageMeta } from '$lib/stores/jdg-temp-store.js';
 	import { upgradeImageMeta } from '$lib/jdg-utils.js';
 
-	import { JDGInputContainer, JDGModal } from '$lib/index.js';
+	import { JDGButton, JDGInputContainer, JDGModal, JDGTextInput } from '$lib/index.js';
+
+	// Bind the hidden fileInput to a custom button for file picking
+	let fileInput;
+
+	const onClickFileUpload = () => {};
 
 	onMount(() => {
 		// Upgrade the image meta
 		draftImageMeta.set(upgradeImageMeta(get(draftImageMeta)));
+		console.log($draftImageMeta);
 	});
 </script>
 
@@ -22,8 +28,55 @@
 	}}
 >
 	<div slot="modal-content-slot">
+		<!-- Read-only values -->
 		<JDGInputContainer label="ID">
 			{$draftImageMeta.id}
+		</JDGInputContainer>
+		<JDGInputContainer label="Version">
+			{$draftImageMeta.version}
+		</JDGInputContainer>
+		<!-- Editable values -->
+		<JDGInputContainer label="Source">
+			<JDGTextInput inputValue={$draftImageMeta.src} />
+			<JDGButton
+				label="Upload..."
+				faIcon="fa-upload"
+				onClickFunction={onClickFileUpload}
+				paddingLeftRight="10px"
+				paddingTopBottom="5px"
+			/>
+			<!-- Hidden file input for button to trigger upload -->
+			<input
+				type="file"
+				style="display: none;"
+				on:change={onClickFileUpload}
+				bind:this={fileInput}
+			/>
+		</JDGInputContainer>
+		<JDGInputContainer label="Title">
+			<JDGTextInput inputValue={$draftImageMeta.title} />
+		</JDGInputContainer>
+		<JDGInputContainer label="Alt">
+			<JDGTextInput inputValue={$draftImageMeta.alt} />
+		</JDGInputContainer>
+		<JDGInputContainer label="Caption">
+			<JDGTextInput inputValue={$draftImageMeta.caption} />
+		</JDGInputContainer>
+		<JDGInputContainer label="Attribution">
+			<JDGTextInput inputValue={$draftImageMeta.attribution} />
+		</JDGInputContainer>
+		<JDGInputContainer label="Show background blur?">
+			<label>
+				<input type="radio" bind:group={$draftImageMeta.showBackgroundBlur} value={true} />
+				Yes
+			</label>
+			<label>
+				<input type="radio" bind:group={$draftImageMeta.showBackgroundBlur} value={false} />
+				No
+			</label>
+		</JDGInputContainer>
+		<JDGInputContainer label="Toolbar alignment">
+			<JDGTextInput inputValue={$draftImageMeta.toolbarAlignment} />
 		</JDGInputContainer>
 	</div>
 </JDGModal>
