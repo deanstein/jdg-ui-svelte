987b85db1ef47a787f3ca4dcd3fa8cb70b8cff89
deanstein <jdeangoldstein@gmail.com>
2025-12-20 00:25:48 -0700

    Revert "Adjust styling on square aspect"
    This reverts commit 73559096f492ef93c2450d47390628080e5fa334.

--- src/lib/components/Input/JDGButton.svelte
+++ src/lib/components/Input/JDGButton.svelte
@@ -75,13 +75,11 @@
 			font-size: ${fontSize};
 			font-family: ${$appFontFamily};
 			width: ${
-				doForceSquareAspect
-					? 'auto'
-					: width == 'fit-content'
-						? 'fit-content'
-						: $isMobileBreakpoint
-							? '100%'
-							: width /* button is 100% width on smallest breakpoint */
+				width == 'fit-content'
+					? 'fit-content'
+					: $isMobileBreakpoint
+						? '100%'
+						: width /* button is 100% width on smallest breakpoint */
 			};
 			border-radius: ${doForceSquareAspect ? '50%' : borderRadius};
 			padding: ${`${paddingTopBottom} ${paddingLeftRight} ${paddingTopBottom} ${paddingLeftRight}`};
