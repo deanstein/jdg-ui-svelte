939e6db430c4aaa9d41b94236a3ab1809f101e2b
deanstein <jdeangoldstein@gmail.com>
2025-12-28 19:21:30 -0700

    Add optional description prop to InputContainer

--- src/lib/components/Input/JDGInputContainer.svelte
+++ src/lib/components/Input/JDGInputContainer.svelte
@@ -1,53 +1,76 @@
 <script>
-	import { jdgColors } from '$lib/jdg-shared-styles.js';
 	import { css } from '@emotion/css';
+	import { jdgColors } from '$lib/jdg-shared-styles.js';
 
+	// Primary label for input component
 	export let label = 'Some Fact or Field Name';
-	export let grow = false;
+	// Optional description
+	export let description = undefined;
+	// Justification for all inputContainer content
 	export let justification = 'left';
+	// Whether the inputContainer should grow to fill available space
+	export let grow = false;
 
-	const labelAndInputContainerCss = css`
+	const inputContainerCss = css`
+		justify-content: ${justification};
 		flex-grow: ${grow ? 1 : 'auto'};
 	`;
 
 	const labelContainercss = css`
 		justify-content: ${justification};
+		margin-bottom: ${description ? '' : '0.5svh'};
 	`;
 
 	const labelCss = css`
 		color: ${jdgColors.text};
 	`;
+
+	const descriptionCss = css`
+		color: ${jdgColors.textLight};
+		text-align: ${justification};
+	`;
 </script>
 
-<div class="label-and-input-container {labelAndInputContainerCss}">
-	<div class="input-content">
+<div class="jdg-input-container {inputContainerCss}">
+	<!-- Title and optional description for input component-->
+	<div class="label-and-description-container">
 		<div class="label-container {labelContainercss}">
 			<div class={labelCss}>
 				{label}
 			</div>
 		</div>
-		<slot />
+		{#if description}
+			<div class="description-container">
+				<div class="description {descriptionCss}">
+					<i class="fa-solid fa-info-circle"></i>&nbsp;&nbsp;{description}
+				</div>
+			</div>
+		{/if}
 	</div>
+	<!-- Slot for the actual input component -->
+	<slot />
 </div>
 
 <style>
-	.label-and-input-container {
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		min-width: 0; /* Allow container to shrink in flex layouts */
-		max-width: 100%; /* Prevent expanding beyond parent */
-		margin-bottom: 1svh;
-	}
-
-	.input-content {
+	.jdg-input-container {
 		display: flex;
 		flex-direction: column;
 		min-height: 2svh;
 		height: 100%;
-		min-width: 0; /* Allow shrinking */
+		width: 100%;
+		min-width: 0; /* Allow container to shrink in flex layouts */
 		max-width: 100%; /* Prevent expanding beyond parent */
+		margin-bottom: 1svh;
 		overflow: hidden;
+	}
+
+	.label-and-description-container {
+		margin-bottom: 0.5svh;
+	}
+
+	.description {
+		font-size: 0.8rem;
+		margin-bottom: 0.5svh;
 	}
 
 	.label-container {
