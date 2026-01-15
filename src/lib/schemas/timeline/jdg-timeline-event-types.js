550d75eac85ba5cb6455c92697ef606f764687a0
deanstein <jdeangoldstein@gmail.com>
2026-01-14 23:46:12 -0700

    Adjust attribution label

--- src/lib/schemas/timeline/jdg-timeline-event-types.js
+++ src/lib/schemas/timeline/jdg-timeline-event-types.js
@@ -37,7 +37,7 @@ const jdgTimelineEventTypes = {
 		additionalContent: {
 			attribution: {
 				default: '',
-				label: 'Attribution',
+				label: 'Quote Attribution',
 				inputType: JDG_INPUT_TYPES.TEXT
 			}
 		}
