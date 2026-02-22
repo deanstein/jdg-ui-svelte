5ab8c51106b1231eb0d40aba677f8538b7007be5
Josh Goldstein <jdeangoldstein@gmail.com>
2026-02-21 19:27:37 -0700

    Demo no explicit exclusion from jumplist - rely on no title instead

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -108,7 +108,7 @@
 			overlapWithHeader={true}
 			parallax
 		/>
-		<JDGContentBoxFloating animateWhenVisible={false} includeInJumpTo={false}>
+		<JDGContentBoxFloating animateWhenVisible={false}>
 			<JDGBodyCopy fontSizeMultiplier={1.15}>
 				<strong>JDG SVELTE UI</strong> is an in-progress UI framework by
 				<a href="./about">Joshua Dean Goldstein.</a>
