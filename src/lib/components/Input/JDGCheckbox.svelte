ece72229abc6b4d437b2f782856c4f675df0c390
Josh Goldstein <jdeangoldstein@gmail.com>
2026-02-25 22:44:11 -0700

    Improve checkbox styling

--- src/lib/components/Input/JDGCheckbox.svelte
+++ src/lib/components/Input/JDGCheckbox.svelte
@@ -12,8 +12,11 @@
 	export let isEnabled = true;
 	export let onCheckAction = () => {};
 	export let onUncheckAction = () => {};
-	// Optional hint text
 	export let hint = undefined;
+	export let checkboxBackgroundColor = jdgColors.active;
+	export let checkColor = 'white';
+
+	const checkboxSize = '1.3em';
 
 	const checkboxWrapperCss = css`
 		justify-content: ${justifyContent};
@@ -23,6 +26,41 @@
 		font-size: ${labelFontSize};
 		line-height: normal;
 		color: ${jdgColors.text};
+	`;
+
+	const checkboxInputCss = css`
+		appearance: none;
+		-webkit-appearance: none;
+		width: ${checkboxSize};
+		height: ${checkboxSize};
+		border: 2px solid ${jdgColors.textLight};
+		border-radius: 3px;
+		background-color: white;
+		cursor: pointer;
+		position: relative;
+		flex-shrink: 0;
+
+		&:checked {
+			background-color: ${checkboxBackgroundColor};
+			border-color: ${checkboxBackgroundColor};
+		}
+
+		&:checked::after {
+			content: '';
+			position: absolute;
+			left: 50%;
+			top: 45%;
+			width: 30%;
+			height: 55%;
+			border: solid ${checkColor};
+			border-width: 0 2.5px 2.5px 0;
+			transform: translate(-50%, -50%) rotate(45deg);
+		}
+
+		&:disabled {
+			opacity: 0.5;
+			cursor: not-allowed;
+		}
 	`;
 
 	$: {
@@ -35,14 +73,21 @@
 </script>
 
 <div class="checkbox-container">
-	<div class="checkbox-wrapper {checkboxWrapperCss}">
-		<input type="checkbox" bind:checked={isChecked} disabled={!isEnabled} class="checkbox" />
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+	<label class="checkbox-wrapper {checkboxWrapperCss}" on:click|stopPropagation>
+		<input
+			type="checkbox"
+			bind:checked={isChecked}
+			disabled={!isEnabled}
+			class="checkbox {checkboxInputCss}"
+		/>
 		{#if showLabel}
-			<div class={checkboxLabelCss}>
+			<span class={checkboxLabelCss}>
 				{label}
-			</div>
+			</span>
 		{/if}
-	</div>
+	</label>
 	<JDGInputHint {hint} justification={justifyContent} showHintIcon={false} />
 </div>
 
@@ -55,10 +100,12 @@
 	.checkbox-wrapper {
 		display: flex;
 		align-items: center;
-		gap: 0.1vw;
+		gap: 0.35em;
+		cursor: pointer;
+		user-select: none;
 	}
 
 	input {
-		margin-left: 0;
+		margin: 0;
 	}
 </style>
